3e66942b4781b233e9e06793f9503c1d6183259c
forworkus <forworkus811@gmail.com>
2024-09-27 00:10:14 -0700

    all files

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+      path: `${__dirname}/public`,
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        }
+      ]
+    }
+  };
